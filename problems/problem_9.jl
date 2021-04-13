76695c73670c14d6326f5177c36180be986f9d7b
Dustin Kenefake <Dustin.Kenefake@tamu.edu>
2021-04-12 22:18:28 -0500

    added problem 9

---
+++ problems/problem_9.jl
@@ -0,0 +1,55 @@
+
+# C Style brute force
+function problem_9(number::Int)::Int
+
+    for i ∈ 1:(number-1)
+        for j ∈ i:(number-1)
+            if i < j && i + j < number
+                k = number - i - j
+                if i^2 + j^2 == k^2
+                    if i + j + k == number
+                        return i*j*k
+                    end
+                end
+            end
+        end
+    end
+    return -1
+end
+
+# C Style not brute force from https://www.mathblog.dk/pythagorean-triplets/
+function problem_9_informed(number::Int)::Int
+    a, b, c = 0, 0, 0
+    s = number
+    m, k, n, d = 0, 0, 0, 0
+
+    limit = isqrt(div(s, 2))
+
+    for m ∈ 2:limit
+        if div(s, 2)% m == 0
+            if m %2 == 0
+                k = m+1
+            else
+                k = m + 2
+            end
+            while k < 2*m && k <= div(s, 2*m)
+                if div(s, 2*m) % k == 0 && gcd(k,m) == 1
+                    d = div(div(s, 2), k*m)
+                    n = k - m
+                    a = d*(m*m-n*n)
+                    b = 2*d*n*m
+                    c = d*(m*m+n*n)
+                    return a*b*c
+                end
+                k += 2
+            end
+        end
+    end
+
+    return -1
+
+end # function
+
+# Benchmarks
+# problem_9(1000)               155.000 μs (0 allocations: 0 bytes)
+# problem_9_informed(1000)      35.815 ns (0 allocations: 0 bytes)
